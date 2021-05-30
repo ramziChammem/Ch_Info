3aa30c98c95b599762bd57e167e4aac27fb46fb2
ramziChammem <ramzichammem69@gmail.com>
2021-05-30 20:41:36 +0100

    init

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Port number
+const port = 3000;
+
+// Connecting to database
+mongoose.connect("mongodb://localhost:27017/Ch_Info");
+
+// on connection
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database successfully!");
+});
+
+// Adding cors middleware
+app.use(cors());
+
+// Using bodyParser
+app.use(bodyParser.json());
+
+// linking server to an html file
+// app.use(express.static(path.join(__dirname, "../public")));
+// Linking needed routes
+// Testing server
+app.get("/", (req, res) => {
+  res.send("Hello!!!");
+});
+
+// App Listening to port
+app.listen(port, () => {
+  console.log("listening on port " + port + "!");
+});
